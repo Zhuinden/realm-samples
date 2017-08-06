63046bc904700287ac690ffa7d0d9a04187607cc
Zhuinden <zhuinden@gmail.com>
2017-08-06 02:22:46 +0200

    adding Realm to project

--- app/src/main/java/io/caster/zhuinden/realmexamples/MainActivity.java
+++ app/src/main/java/io/caster/zhuinden/realmexamples/MainActivity.java
@@ -3,11 +3,23 @@ package io.caster.zhuinden.realmexamples;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import io.realm.Realm;
+
 public class MainActivity
         extends AppCompatActivity {
+    Realm realm;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        realm = Realm.getDefaultInstance();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        realm.close();
+        realm = null;
     }
 }
