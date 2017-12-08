a15cad44e71488241f8740bb4987784e93d664d5
Zhuinden <zhuinden@gmail.com>
2017-12-08 20:53:03 +0100

    Installing Realm in a Kotlin project

--- app/src/main/java/io/caster/zhuinden/realmexamples/MainActivity.kt
+++ app/src/main/java/io/caster/zhuinden/realmexamples/MainActivity.kt
@@ -2,10 +2,20 @@ package io.caster.zhuinden.realmexamples
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import io.realm.Realm
 
 class MainActivity : AppCompatActivity() {
+    var realm: Realm? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        realm = Realm.getDefaultInstance()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        realm?.close()
+        realm = null
     }
 }
