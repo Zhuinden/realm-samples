8a88f94680f35b8601441f6649239183cbb25c6e
Zhuinden <zhuinden@gmail.com>
2017-12-11 02:54:32 +0100

    Add Queries using io.realm.kotlin + RealmFieldNamesHelper

--- app/src/main/java/io/caster/zhuinden/realmexamples/MainActivity.kt
+++ app/src/main/java/io/caster/zhuinden/realmexamples/MainActivity.kt
@@ -2,7 +2,10 @@ package io.caster.zhuinden.realmexamples
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import io.realm.Case
 import io.realm.Realm
+import io.realm.kotlin.where
 
 class MainActivity : AppCompatActivity() {
     var realm: Realm? = null
@@ -10,7 +13,38 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        realm = Realm.getDefaultInstance()
+        val realm = Realm.getDefaultInstance()
+        this.realm = realm
+        val persons = realm.where<Person>().lessThan(PersonFields.NAME, 40).findAll()
+        persons.forEach {
+            Log.i(this@MainActivity::class.java.name, "Person name: ${it.name}")
+        }
+        val others = realm.where<Person>()
+                .beginGroup()
+                .beginsWith(PersonFields.NAME, "John", Case.INSENSITIVE)
+                .and()
+                .greaterThan(PersonFields.AGE, 6)
+                .endGroup()
+                .or()
+                .contains(PersonFields.NAME, "Mary", Case.INSENSITIVE)
+                .findAll()
+        others.forEach {
+            Log.i(this@MainActivity::class.java.name, "Others person name: ${it.name}")
+        }
+
+        val linked = realm.where<Person>()
+                .greaterThan(PersonFields.PARENTS.AGE, 7)
+                .findAll()
+        linked.forEach {
+            Log.i(this@MainActivity::class.java.name, "Linked person name: ${it.name}")
+        }
+
+        val backlinked = realm.where<Person>()
+                .lessThan("${PersonFields.CHILDREN}.${PersonFields.AGE}", 15)
+                .findAll()
+        backlinked.forEach {
+            Log.i(this@MainActivity::class.java.name, "Backlinked person name: ${it.name}")
+        }
     }
 
     override fun onDestroy() {
