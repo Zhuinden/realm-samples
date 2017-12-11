895faba08fc1fda7d594bc74cbde141ec78ad3d5
Zhuinden <zhuinden@gmail.com>
2017-12-11 03:22:45 +0100

    Remove previous queries, add single-element queries for debug

--- app/src/main/java/io/caster/zhuinden/realmexamples/MainActivity.kt
+++ app/src/main/java/io/caster/zhuinden/realmexamples/MainActivity.kt
@@ -3,7 +3,6 @@ package io.caster.zhuinden.realmexamples
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
-import io.realm.Case
 import io.realm.Realm
 import io.realm.kotlin.where
 
@@ -15,41 +14,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val realm = Realm.getDefaultInstance()
         this.realm = realm
-        val persons = realm.where<Person>().lessThan(PersonFields.AGE, 40).findAll()
-        persons.forEach {
-            Log.i(this@MainActivity::class.java.name, "Person name: ${it.name}")
-        }
-        val others = realm.where<Person>()
-                .beginGroup()
-                .beginsWith(PersonFields.NAME, "John", Case.INSENSITIVE)
-                .and()
-                .greaterThan(PersonFields.AGE, 6)
-                .endGroup()
-                .or()
-                .contains(PersonFields.NAME, "Mary", Case.INSENSITIVE)
-                .findAll()
-        others.forEach {
-            Log.i(this@MainActivity::class.java.name, "Others person name: ${it.name}")
-        }
-        val filteredOthers = others.where().contains(PersonFields.NAME, "Mom", Case.INSENSITIVE).findAll()
-        filteredOthers.forEach {
-            Log.i(this@MainActivity::class.java.name, "Filtered others person name: ${it.name}")
-        }
-
-        val linked = realm.where<Person>()
-                .greaterThan(PersonFields.PARENTS.AGE, 7)
-                .findAll()
-        linked.forEach {
-            Log.i(this@MainActivity::class.java.name, "Linked person name: ${it.name}")
-        }
-
-        val backlinked = realm.where<Person>()
-                .lessThan("${PersonFields.CHILDREN}.${PersonFields.AGE}", 15)
-                .findAll()
-        backlinked.forEach {
-            Log.i(this@MainActivity::class.java.name, "Backlinked person name: ${it.name}")
-        }
-
+        val persons = realm.where<Person>().findAll()
+        val person1 = realm.where<Person>().equalTo(PersonFields.NAME, "John Doe").findFirst()
+        val person2 = realm.where<Person>().equalTo(PersonFields.NAME, "Mary Sue").findFirst()
+        val person3 = realm.where<Person>().equalTo(PersonFields.NAME, "John's Dad").findFirst()
+        val person4 = realm.where<Person>().equalTo(PersonFields.NAME, "John's Mom").findFirst()
+        val person5 = realm.where<Person>().equalTo(PersonFields.NAME, "Mary's Dad").findFirst()
+        val person6 = realm.where<Person>().equalTo(PersonFields.NAME, "Mary's Mom").findFirst()
+        val person7 = realm.where<Person>().equalTo(PersonFields.NAME, "John's and Mary's Son").findFirst()
+        Log.i(this::class.java.name, "DEBUG")
     }
 
     override fun onDestroy() {
