5a0d2c4fbf9b0c15a930f27bf54a0534dd421346
Zhuinden <zhuinden@gmail.com>
2017-12-11 02:34:00 +0100

    Add Person object as an example

---
+++ app/src/main/java/io/caster/zhuinden/realmexamples/Person.kt
@@ -0,0 +1,39 @@
+package io.caster.zhuinden.realmexamples
+
+import io.realm.RealmList
+import io.realm.RealmObject
+import io.realm.RealmResults
+import io.realm.annotations.Index
+import io.realm.annotations.LinkingObjects
+import io.realm.annotations.PrimaryKey
+import java.util.*
+
+/**
+ * Created by Zhuinden on 2017.12.11..
+ */
+open class Person : RealmObject() {
+    @PrimaryKey
+    var id: String? = null
+
+    @Index
+    var name: String? = null
+
+    @Index
+    var age: Int = 0 // implicitly @Required
+
+    var birthday: Date? = null
+
+    var image: ByteArray? = null
+
+    var spouse: Person? = null
+
+    var phoneNumbers: RealmList<String>? = null
+
+    var parents: RealmList<Person>? = null
+
+    @LinkingObjects("spouse")
+    val spouseOf: RealmResults<Person>? = null
+
+    @LinkingObjects("parents")
+    val children: RealmResults<Person>? = null
+}
